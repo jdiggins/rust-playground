a81606a92fa40b085dc82a4a4d69e84f2a98a77f
sniggidnhoj <j.c.diggins@gmail.com>
2018-10-08 20:19:16 -0400

    mutation, shadowing, variables

---
+++ variable_mutating.rs
@@ -0,0 +1,15 @@
+fn main() {
+    const MAX: u32 = 100_000;
+    println!("The value of MAX is: {}", MAX);
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 4;
+    println!("The value of x is: {}", x);
+    x += 1;
+    println!("The value of x is: {}", x);
+    let x = "asdf";
+    let mut x = x.len();
+    println!("The value of x is: {}", x);
+    x = 2;
+    println!("The value of x is: {}", x);
+}
