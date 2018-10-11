fac47e84de175a072862547eab5a7c110c74b9a3
sniggidnhoj <j.c.diggins@gmail.com>
2018-10-11 11:56:31 -0400

    Working with structs

---
+++ structs.rs
@@ -0,0 +1,84 @@
+/* John Diggins
+ * 10/11/18
+ * Structs!
+ */
+ 
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+
+
+/* User building function */
+fn build_user(email: String, username: String) -> User {
+    User {
+        email, /* Short for email: email, whens when var and field same name */
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+#[derive(Debug)] /* needed to use println! on struct */
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+fn main() {
+
+    let user1 = User {
+        email: String::from("user1@user1.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    /* Create new user with some info from user1
+    * Could be useful when updating user email / username */
+    let user2 = User {
+        email: String::from("newemail@email.com"),
+        username: String::from("newusername"),
+        ..user1
+    };
+
+    let user3 = build_user(String::from("user3@email.com"), String::from("user3!!"));
+
+    println!("User1 username: {} \nUser2 username: {} \nUser3 username: {}",
+        user1.username, user2.username, user3.username);
+
+    /* Tuple structs without names */
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let rect1 = (30, 50);
+    let rect2 = Rectangle { width: 30, height: 50 };
+    println! (
+        "The area of the rectangle is {} square pixels.",
+        area(rect1)
+    );
+
+    println!("rect2 is {:?}", rect2);
+    /* This will print:
+     * rect 2 is Rectangle { width: 30, height: 50}
+     */
+
+    println!("rect2 is {:#?}", rect2);
+    /* This will print:
+     * rect 2 is Redtangle {
+     *   width: 30,
+     *   height: 50
+     * }
+     */
+
+}
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
