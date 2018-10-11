389efedf8f92af76f3c31d0f8b1630cf63b4a7e9
sniggidnhoj <j.c.diggins@gmail.com>
2018-10-11 14:41:35 -0400

    Added some methods

--- structs.rs
+++ structs.rs
@@ -28,6 +28,17 @@ struct Rectangle {
     height: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
 fn main() {
 
     let user1 = User {
@@ -64,18 +75,30 @@ fn main() {
         area(rect1)
     );
 
-    println!("rect2 is {:?}", rect2);
     /* This will print:
      * rect 2 is Rectangle { width: 30, height: 50}
      */
-
-    println!("rect2 is {:#?}", rect2);
+    println!("rect2 is {:?}", rect2);
+   
     /* This will print:
      * rect 2 is Redtangle {
      *   width: 30,
      *   height: 50
      * }
      */
+    println!("rect2 is {:#?}", rect2);
+    
+
+    /* Called using method (internal function) */
+     println!("The area of rect2 is {}.",
+        rect2.area());
+
+    let rect3 = Rectangle { width: 10, height: 40 };
+    let rect4 = Rectangle { width: 50, height: 50 };
+
+    println!("Can rect2 hold rect3? {}", rect2.can_hold(&rect3));
+    println!("Can rect2 hold rect4? {}", rect2.can_hold(&rect4));
+    
 
 }
 
